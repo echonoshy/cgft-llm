8f2d6d3570c6b78f42142d837689c3cc71d9b861
Lake <56507185+echonoshy@users.noreply.github.com>
2024-06-28 23:39:52 +0800

    feat: add ollama_index.py

---
+++ llama-index/ollama_index.py
@@ -0,0 +1,15 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+
+documents = SimpleDirectoryReader("data").load_data()
+
+# bge-base embedding model
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+
+# ollama
+Settings.llm = Ollama(model="llama3", request_timeout=360.0)
+
+index = VectorStoreIndex.from_documents(
+    documents,
+)
